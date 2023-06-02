3df7d90105913291a1fadc292f31c3489be7daf0
Alex Boten <aboten@lightstep.com>
2023-06-02 10:00:39 -0700

    example using the python layer
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- handler.py
+++ handler.py
@@ -1,20 +1,8 @@
 import json
 
 from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    SimpleSpanProcessor,
-)
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
 def hello(event, context):
-    provider = TracerProvider()
-    processor = SimpleSpanProcessor(OTLPSpanExporter())
-    provider.add_span_processor(processor)
-
-    # Sets the global default tracer provider
-    trace.set_tracer_provider(provider)
-
     # Creates a tracer from the global tracer provider
     tracer = trace.get_tracer("my.tracer.name")
     with tracer.start_as_current_span("span-name") as span:
